ceae5de8f41b20ad46f80ef9c138f496a04d9fc4
graphicedit <graphicedit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-06 01:58:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/end-page-slide-box/trunk@459803 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- end-page-slide-box.php
+++ end-page-slide-box.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: End Page Slide Box
-Plugin URI: http://www.graphicedit.com/
+Plugin URI: http://graphicedit.com/blog/plugin/end-page-slide-box/
 Description: End Page Slide Box
 Version: 1.0.0
 Author: GraphicEdit
